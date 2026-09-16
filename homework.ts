cf0dcc46127286c07f67fb71e00111360ab3b699
Dantewar <warda@live.se>
2026-09-16 09:17:37 +0200

    Add enum exercises

--- homework.ts
+++ homework.ts
@@ -96,3 +96,57 @@ const printCar = (car: Car): string => {
 };
 
 console.log(printCar(car));
+
+
+// Skill 3: Enums
+
+enum Color {
+  Red,
+  Green,
+  Blue
+}
+
+const showColor = (color: Color): string => {
+  return `You chose ${Color[color]}`;
+};
+
+console.log(showColor(Color.Red));
+console.log(showColor(Color.Green));
+console.log(showColor(Color.Blue));
+
+
+enum PizzaSize {
+  Small,
+  Medium,
+  Large
+}
+
+const orderPizza = (size: PizzaSize): string => {
+  return `You ordered a ${PizzaSize[size]} pizza.`;
+};
+
+console.log(orderPizza(PizzaSize.Small));
+console.log(orderPizza(PizzaSize.Medium));
+console.log(orderPizza(PizzaSize.Large));
+
+
+enum Role {
+  Admin,
+  User,
+  Guest
+}
+
+const printRole = (role: Role): string => {
+  switch (role) {
+    case Role.Admin:
+      return "You have full access";
+    case Role.User:
+      return "You have limited access";
+    case Role.Guest:
+      return "You have guest access";
+  }
+};
+
+console.log(printRole(Role.Admin));
+console.log(printRole(Role.User));
+console.log(printRole(Role.Guest));
